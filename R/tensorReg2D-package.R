1fd18d4601b1c09c0cfac79436c6d5c113f3e13d
Ping-Yang Chen <pychen.ping@gmail.com>
2021-12-02 15:54:21 +0800

    renamed from TensorTest2D

---
+++ R/tensorReg2D-package.R
@@ -0,0 +1,32 @@
+#'
+#' @title The tensorReg2D Package
+#'
+#' @description \kbd{tensorReg2D} is a tool used to fit second-order tensor type data.
+#'
+#' @details
+#' This package contains functions for fitting generalized linear models on second-order tensor type data.
+#'
+#' It mainly focus on parameter estimation, including parameter coefficients and standard deviation.
+#'
+#' Currently, this package includes generalized linear models with \kbd{identity link} (Regression),
+#' \kbd{logit link} (Logistic regression), and \kbd{log link} (Poisson regression).
+#'
+#' For a complete list of functions, use \kbd{help(package = tensorReg2D)}.
+#'
+#' @docType package
+#'
+#' @author
+#' Sheng-Mao Chang \email{smchang@mail.ncku.edu.tw}
+#'
+#' Wenbin Lu \email{wlu4@ncsu.edu}
+#'
+#' Jung-Ying Tzeng \email{jytzeng@ncsu.edu}
+#' 
+#' Ping-Yang Chen \email{pychen.ping@gmail.com}
+#'
+#' Maintainer: Mark Chen \email{l501l501l@gmail.com}
+#'
+#' @name tensorReg2D
+#' @aliases tensorReg2D
+#'
+NULL
